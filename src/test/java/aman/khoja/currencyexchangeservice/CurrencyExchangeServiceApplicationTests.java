7666c6f1a98f0cfa7d2a557f24a45466f3768cea
Aman Khoja <aak171730@utdallas.edu>
2022-11-26 01:35:43 -0600

    intial setup

---
+++ src/test/java/aman/khoja/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package aman.khoja.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
