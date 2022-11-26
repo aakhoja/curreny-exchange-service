331ae52d30186813845060ccf5f4adeeeb6a6c21
Aman Khoja <aak171730@utdallas.edu>
2022-11-26 15:36:20 -0600

    added sql script for data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into curreny_exchange
+(id,conversion_rate,environment,curreny_from,curreny_to)
+values(1001,70,'','USD','INR');
+insert into curreny_exchange
+(id,conversion_rate,environment,curreny_from,curreny_to)
+values(1002,85,'','EUR','INR');
+insert into curreny_exchange
+(id,conversion_rate,environment,curreny_from,curreny_to)
+values(1003,40,'','AUD','INR');
