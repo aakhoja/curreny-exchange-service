34e69acac4e7dc0a992d121144b3908f5538a92b
Aman Khoja <aak171730@utdallas.edu>
2022-11-26 02:31:12 -0600

    added spring jpa and h2 database functionality

--- src/main/java/aman/khoja/currencyexchangeservice/controller/CurrenyExchange.java
+++ src/main/java/aman/khoja/currencyexchangeservice/controller/CurrenyExchange.java
@@ -1,10 +1,18 @@
 package aman.khoja.currencyexchangeservice.controller;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
+@Entity
 public class CurrenyExchange {
+
+    @Id
     private Long id;
+    @Column(name = "curreny_from")
     private String from;
+    @Column(name = "curreny_to")
     private String to;
     private BigDecimal conversionRate;
     private String environment;
